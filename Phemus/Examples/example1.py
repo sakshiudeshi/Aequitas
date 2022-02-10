5b1dfd118cca9a4a2c056f96a2b22c1c25aa6d76
Yemi Shin <shiny@carleton.edu>
2022-02-09 18:24:41 -0600

    bug fixes, making arrays consistent across everything

--- Phemus/Examples/example1.py
+++ Phemus/Examples/example1.py
@@ -1,15 +1,20 @@
-from Phemus import *
+import os, sys
+os.chdir('..')
+sys.path.append(os.getcwd())
+from src.Phemus import *
+#from Phemus import * <-- use this if loading Phemus from local folder
 
 def main():
-    num_param = 8
+    num_params = 8
     sensitive_param_idx = 5 # Starts at O
     sensitive_param_name = "Gender"
     col_to_be_predicted = "LeaveOrNot"
     dataset_dir = "Employee.csv"
     model_type = "DecisionTree"
+    os.chdir('Examples')
+    sys.path.append(os.getcwd())
 
-    dataset = Dataset(num_param, sensitive_param_idx, model_type, \
-        sensitive_param_name, col_to_be_predicted, dataset_dir)
+    dataset = Dataset(num_params=num_params, sensitive_param_idx=sensitive_param_idx, model_type=model_type, sensitive_param_name=sensitive_param_name, col_to_be_predicted=col_to_be_predicted, dataset_dir=dataset_dir)
 
     pkl_dir = 'Employee_DecisionTree_Original.pkl'
     improved_pkl_dir = 'Employee_DecisionTree_Original_Improved.pkl'
@@ -25,9 +30,11 @@ def main():
     num_trials = 100
     samples = 100
 
-    run_aequitas_fully_direct(dataset, perturbation_unit, cleaned_csv_dir, pkl_dir, \
-                    improved_pkl_dir, retrain_csv_dir, plot_dir, threshold,  \
-                    global_iteration_limit, local_iteration_limit, num_trials, samples)
+    # run_aequitas_once(dataset, perturbation_unit, cleaned_csv_dir, pkl_dir, \
+    #                 improved_pkl_dir, retrain_csv_dir, plot_dir, "Random", threshold,  \
+    #                 global_iteration_limit, local_iteration_limit, num_trials, samples)
 
+    run_aequitas_once(dataset=dataset, perturbation_unit=perturbation_unit, pkl_dir=pkl_dir, improved_pkl_dir=improved_pkl_dir, retrain_csv_dir=retrain_csv_dir, plot_dir=plot_dir, mode="Fully")
+    
 if __name__ == "__main__":
     main() 
