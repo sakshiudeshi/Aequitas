e190e3a6ad28aa5bf74ab6b8229ad11f9cd9495b
Yemi Shin <shiny@carleton.edu>
2022-02-10 15:43:57 -0600

    a new Run_Aequitas.py with correct capitalization

---
+++ Phemus/src/Phemus/Run_Aequitas.py
@@ -0,0 +1,33 @@
+from .Retrain_Sklearn import retrain_sklearn
+from .Aequitas_Fully_Directed_Sklearn import aequitas_fully_directed_sklearn
+from .Aequitas_Semi_Directed_Sklearn import aequitas_semi_directed_sklearn
+from .Aequitas_Random_Sklearn import aequitas_random_sklearn
+from .Generate_Sklearn_Classifier import generate_sklearn_classifier
+
+from .Dataset import Dataset
+
+def run_aequitas_once(dataset, perturbation_unit, pkl_dir, improved_pkl_dir, retrain_csv_dir, plot_dir, mode = "Random", threshold = 0, global_iteration_limit = 1000, local_iteration_limit = 100, num_trials = 100, samples = 100):
+    if mode == "Random":
+        aequitas_random_sklearn(dataset, perturbation_unit, threshold, global_iteration_limit, local_iteration_limit, pkl_dir, retrain_csv_dir)
+    elif mode == "Semi":
+        aequitas_semi_directed_sklearn(dataset, perturbation_unit, threshold, global_iteration_limit, local_iteration_limit, pkl_dir, retrain_csv_dir)
+    elif mode == "Fully":
+        aequitas_fully_directed_sklearn(dataset, perturbation_unit, threshold, global_iteration_limit, local_iteration_limit, pkl_dir, retrain_csv_dir)
+    else:
+        raise ValueError("Mode of Aequitas selected is not valid. Possible modes are Random, Semi and Fully")
+    retrain_sklearn(dataset, pkl_dir, retrain_csv_dir, improved_pkl_dir, plot_dir, num_trials, samples)
+
+def run_aequitas(dataset: Dataset, perturbation_unit, cleaned_csv_dir, pkl_dir, improved_pkl_dir, retrain_csv_dir, plot_dir, \
+        mode = "Random", threshold = 0, global_iteration_limit = 1000, local_iteration_limit = 100, num_trials = 100, samples = 100):
+
+    num_of_sens_param = len(dataset.sensitive_param_idx_list)
+
+    generate_sklearn_classifier(dataset, cleaned_csv_dir, pkl_dir)
+
+    for i in range(num_of_sens_param):
+        dataset.update_sensitive_parameter(dataset.sensitive_param_idx_list[i])
+        run_aequitas_once(dataset, perturbation_unit, cleaned_csv_dir, pkl_dir, improved_pkl_dir, \
+            retrain_csv_dir, plot_dir, mode, threshold, \
+            global_iteration_limit, local_iteration_limit, num_trials, samples)
+
+
